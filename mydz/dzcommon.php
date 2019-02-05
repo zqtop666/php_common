845c25b53d62542ddcee353dd29a5a089f146518
zhangqiang <zq.top.999@gmail.com>
2019-02-05 13:05:08 +0800

    维护

--- mydz/dzcommon.php
+++ mydz/dzcommon.php
@@ -59,13 +59,13 @@ function showDialog($isparentWindow, $msg, $mode, $title = '', $func = 'null', $
     exit();
 }
 
-    // 定义当前请求的系统常量
-    define('NOW_TIME',      $_SERVER['REQUEST_TIME']);
-    define('REQUEST_METHOD',$_SERVER['REQUEST_METHOD']);
-    define('IS_GET',        REQUEST_METHOD =='GET' ? true : false);
-    define('IS_POST',       REQUEST_METHOD =='POST' ? true : false);
-    define('IS_PUT',        REQUEST_METHOD =='PUT' ? true : false);
-    define('IS_DELETE',     REQUEST_METHOD =='DELETE' ? true : false);
-    define('IS_AJAX',       ((isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') || !empty($_POST['ajax']) || !empty($_GET['ajax'])) ? true : false);
-    define('IS_PJAX',       array_key_exists('HTTP_X_PJAX', $_SERVER) && $_SERVER['HTTP_X_PJAX']);
-    define('HTTP_HOST', $_SERVER['HTTP_HOST']);
+// 定义当前请求的系统常量
+define('NOW_TIME', $_SERVER['REQUEST_TIME']);
+define('REQUEST_METHOD', $_SERVER['REQUEST_METHOD']);
+define('IS_GET', REQUEST_METHOD == 'GET' ? true : false);
+define('IS_POST', REQUEST_METHOD == 'POST' ? true : false);
+define('IS_PUT', REQUEST_METHOD == 'PUT' ? true : false);
+define('IS_DELETE', REQUEST_METHOD == 'DELETE' ? true : false);
+define('IS_AJAX', ((isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') || !empty($_POST['ajax']) || !empty($_GET['ajax'])) ? true : false);
+define('IS_PJAX', array_key_exists('HTTP_X_PJAX', $_SERVER) && $_SERVER['HTTP_X_PJAX']);
+define('HTTP_HOST', $_SERVER['HTTP_HOST']);
