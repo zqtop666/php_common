b930fd5d15f70085441f651e0ab7516451430127
zhangqiang <1702963@qq.com>
2019-11-23 12:00:56 +0800

    埋代码_模板分析

--- myphp/mytpltool.php
+++ myphp/mytpltool.php
@@ -1,10 +1,11 @@
 <?php
 /*
-* 部署方法：
-* 1、在模板编译方法中埋 $content.=zq_page($from)  (记得判断方法exists)，
-* 2、在模板引擎方法中加入：(defined('ZQ_PAGE') && ZQ_PAGE) 为真时强行调用模板编译方法。
-*/
-define('ZQ_PAGE', 0);
+ * 部署方法：
+ * 1、在模板编译方法中埋zq_page($from)  (记得判断方法exists)，
+ * 2、在模板引擎方法中加入：(defined('ZQ_PAGE') && ZQ_PAGE) 为真时强行调用模板编译方法。
+ */
+define('ZQ_PAGE', 1);
+define('ZQ_ECHO', 1);
 
 if (!function_exists("zq_var")) {
     function zq_var($source)
@@ -16,7 +17,7 @@ if (!function_exists("zq_var")) {
 if (!function_exists("zq_page")) {
     function zq_page($source)
     {
-        if (defined('ZQ_PAGE') && ZQ_PAGE) {
+        if (defined('ZQ_ECHO') && ZQ_ECHO) {
             $str = "<zqs style='display:none;position:absolute;height:0;overflow:hidden;'>" . $source . "</zqs>";
             $str .= "<zqs style='display:none;position:absolute;height:0;overflow:hidden;'>" . var_export(get_included_files(), 'true') . "</zqs>";
             $str = str_replace("\n", "\\", $str);
