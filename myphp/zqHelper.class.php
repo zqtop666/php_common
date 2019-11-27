99118f9d6e8324068a663281992f72149241c72a
zhangqiang <1702963@qq.com>
2019-11-27 21:20:36 +0800

    加入随机串

--- myphp/zqHelper.class.php
+++ myphp/zqHelper.class.php
@@ -697,6 +697,17 @@ class zqHelper
         }
         return $result;
     }
+    function str_getRandomString($len, $chars = null)
+    {
+        if (is_null($chars)) {
+            $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        }
+        mt_srand(10000000 * (double)microtime());
+        for ($i = 0, $str = '', $lc = strlen($chars) - 1; $i < $len; $i++) {
+            $str .= $chars[mt_rand(0, $lc)];
+        }
+        return $str;
+    }
 }
 
 class zqHelperOldPhp
